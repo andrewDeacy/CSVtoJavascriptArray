edd58209468a9b0673468e42bb97ba0e6537b284
unknown <adeacy@ALGRVWIN785.ALGWORLDWIDE.local>
2014-11-11 12:42:14 -0600

    Now works using user input for a given amount of array values and value names

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
 __author__ = 'Andrew'
-
-#quick tool to create a functonal javascript array from a raw csv file format
+#quick tool to create a functional javascript array from a raw csv file format
 import csv
 import sys
 
-csvfile = open('data.csv', 'rt')
-reader = csv.reader(csvfile)
-for row in reader:
-    print ("{City: '" + row[0] + "', Index: " + row[1] + ", LivingIndex: " + row[2] + "},")
+isValid = 0
+columns = []
 
+while isValid < 1:
+    try:
+        number = input('Enter the amount of columns in the array: ')
+        if int(number) > 0:
+            for num in range(0,int(number)):
+                name = input('Enter the name for each column: ')
+                columns.append(name)
+                isValid = 2
+        else:
+            isValid = 0
+    except:
+        print ('Error enter a valid number please...')
+isValid = 0
+
+length = int(len(columns))
+
+while isValid < 1:
+    csvfile = open('data.csv', 'rt')
+    reader = csv.reader(csvfile)
+
+    for row in reader:
+        print('{', end="")
+        #need to find out if row is a int or string to determine if i need quotes
+        for num in range(0, length):
+            if (num) == (length -1):
+               print (columns[num] + ": " "'" + row[num] + "'", end="")
+            else:
+               print (columns[num] + ": " "'" + row[num] + "',", end="")
+        print('}')
