6202a2becb56d2596801d29ce2996ebabeb5207d
andrewdeacy <adeacydevelopment@gmail.com>
2014-11-10 22:23:30 -0600

    Data for drewslist converted properly

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ __author__ = 'Andrew'
 
 #quick tool to create a functonal javascript array from a raw csv file format
 import csv
+import sys
+
+csvfile = open('data.csv', 'rt')
+reader = csv.reader(csvfile)
+for row in reader:
+    print ("{City: '" + row[0] + "', Index: " + row[1] + ", LivingIndex: " + row[2] + "},")
 
